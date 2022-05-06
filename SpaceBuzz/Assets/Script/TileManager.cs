dc929b547a787a48b90bbba44a9232aedbf17282
mishaDevy <unalgirayergun@gmail.com>
2022-05-06 22:24:48 +0300

    endlessness

---
+++ SpaceBuzz/Assets/Script/TileManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    public float tileLength = 19;
+    public int maxTiles = 5;
+
+    public Transform playerTransform;
+
+    private List<GameObject> activeTiles = new List<GameObject>();
+    private void Start()
+    {
+        for(int i=0;i<maxTiles; i++)
+        {
+            if(i == 0)
+            {
+                SpawnTile(0);
+            }
+            else
+            {
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+            }
+            
+        }
+    }
+    private void Update()
+    {
+        if (playerTransform.position.z -25 > zSpawn - (maxTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject go=Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(go);
+        zSpawn += tileLength;
+    }
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
