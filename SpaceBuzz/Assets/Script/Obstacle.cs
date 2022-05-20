58585f353e5a1ae52e2498e48a0809258bc1c937
Fatih Şeker <fatihseker0@hotmail.com>
2022-05-20 16:03:53 +0300

    Ekosistem duzeltildi

--- SpaceBuzz/Assets/Script/Obstacle.cs
+++ SpaceBuzz/Assets/Script/Obstacle.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] GameObject UI;
- 
+
+    [SerializeField] private CharacterController _controller;
+
+
+    [SerializeField] private LevelDistance _distance; // Ekosistem için eklendi
+    [SerializeField] private Animator _anim; // Ekosistem için eklendi
+
     private void LateUpdate()
     {
         OxygenZero();
@@ -17,7 +23,13 @@ public class Obstacle : MonoBehaviour
         if (Oxygen.oxygenCylinder <= 0)
         {
             UI.SetActive(true);
-            Time.timeScale = 0f;
+            Time.timeScale = 1f;
+            
+            // Ekosistemi devam ettirmek için kullanýlanlar
+            _controller.enabled = false;
+            _distance.enabled = false;
+            _anim.SetBool("run", false);
+            _anim.SetBool("idle", true);
         }
     }
 }
