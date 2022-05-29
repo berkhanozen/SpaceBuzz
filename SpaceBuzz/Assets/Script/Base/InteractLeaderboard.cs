74ab8d0fb5426aff0c364b65f0b4589e6add550d
mishaDevy <unalgirayergun@gmail.com>
2022-05-29 19:15:16 +0300

    UI yeniden boyutlandırma , O2_UI eklemesi ve Splash Screen

--- SpaceBuzz/Assets/Script/Base/InteractLeaderboard.cs
+++ SpaceBuzz/Assets/Script/Base/InteractLeaderboard.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class InteractLeaderboard : MonoBehaviour
 {
     public GameObject leaderboardUI;
+    private bool CallMeOnce=true;
 
     private void OnTriggerStay(Collider other)
     {
-        leaderboardUI.SetActive(true);
+        if (CallMeOnce == true)
+        {
+            leaderboardUI.SetActive(true);
+            CallMeOnce = false;
+        }
+        
     }
 
     private void OnTriggerExit(Collider other)
+    {
+        leaderboardUI.SetActive(false);
+        CallMeOnce = true;
+    }
+
+    public void closeLeaderboardUI()
     {
         leaderboardUI.SetActive(false);
     }
