08651f5fcdfc437d4bb2591d16d596f46194a76c
Berkhan Özen <berkhanozen@gmail.com>
2022-09-26 19:26:16 +0300

    Input system debugging

--- SpaceBuzz/Assets/Script/CharacterController.cs
+++ SpaceBuzz/Assets/Script/CharacterController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,23 +7,19 @@ using UnityEngine.InputSystem.EnhancedTouch;
 
 public class CharacterController : MonoBehaviour
 {
+    public float moveSpeed;
+    public float leftRightSpeed;
 
     [SerializeField] private Animator anim;
+    [SerializeField] private Vector2 verticalLimit = new Vector2(-2, 2);
 
-
+    private Vector2 direction;
+    
     private void Start()
     {
         anim.SetBool("run", true);
         Application.targetFrameRate = 120;
     }
-
-    
-    public float moveSpeed;
-    public float leftRightSpeed;
-
-    private Vector3 _touchDown;
-    private Vector3 _touchUp;
-
 
     private void OnEnable()
     {
@@ -44,52 +41,19 @@ public class CharacterController : MonoBehaviour
 
         foreach (var touch in activeTouches)
         {
-            if (touch.began)
+            if (touch.phase == UnityEngine.InputSystem.TouchPhase.Moved)
             {
-                _touchDown = touch.screenPosition;
-                _touchUp = touch.startScreenPosition;
-            }
-            else if (touch.phase == UnityEngine.InputSystem.TouchPhase.Moved)
-            {
-                _touchDown = touch.screenPosition;
-
-                if ((gameObject.transform.position.x > LevelBoundary.leftSide) && (gameObject.transform.position.x < LevelBoundary.rightSide))
-                {
-                    transform.Translate(CalculateDirection() * leftRightSpeed * Time.deltaTime, 0, 0);
-                }
-                else if (gameObject.transform.position.x < LevelBoundary.leftSide)
-                {
-                    if (CalculateDirection() > 0)
-                    {
-                        transform.Translate(CalculateDirection() * leftRightSpeed * Time.deltaTime, 0, 0);
-                    }
-                }
-                else if (gameObject.transform.position.x > LevelBoundary.rightSide)
-                {
-                    if (CalculateDirection() < 0)
-                    {
-                        transform.Translate(CalculateDirection() * leftRightSpeed * Time.deltaTime, 0, 0);
-                    }
-                }
-            }
-            else if (touch.phase == UnityEngine.InputSystem.TouchPhase.Stationary)
-            {
-                _touchUp = touch.screenPosition;
-            }
-            else if (touch.ended)
-            {
-                _touchDown = Vector3.zero;
-                _touchUp = Vector3.zero;
+                direction = touch.delta;
+                Move();
             }
         }
     }
 
-
-    float CalculateDirection()
+    void Move()
     {
-        Vector3 temp = (_touchDown - _touchUp).normalized;
-        float tempX = temp.x;
-        return tempX;
+        var newXPos = transform.position.x + leftRightSpeed * direction.x * Time.deltaTime;
+        newXPos = Mathf.Clamp(newXPos, verticalLimit.x, verticalLimit.y);
+        transform.position = new Vector3(newXPos, transform.position.y, transform.position.z);
     }
 
 }
--- SpaceBuzz/Assets/Script/Distance/LevelDistance.cs
+++ SpaceBuzz/Assets/Script/Distance/LevelDistance.cs
@@ -33,49 +33,41 @@ public class LevelDistance : MonoBehaviour
         if (Idistance == 50) 
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 100)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 150)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 200)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 250)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 300)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 400)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 500)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
             if (_questcheck != null)
             {
                 _questcheck.quest1 = true;
@@ -86,29 +78,28 @@ public class LevelDistance : MonoBehaviour
         if (Idistance == 600)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 700)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 850)
         {
             _controller.moveSpeed += 2;
-            _controller.leftRightSpeed += 0.2f;
         }
 
         if (Idistance == 1000)
         {
+            _controller.moveSpeed += 2;
             _questcheck.quest2 = true;
             PlayerPrefs.SetInt("quest2", (_questcheck.quest2 ? 1 : 0));
         }
 
         if (Idistance == 2000)
         {
+            _controller.moveSpeed += 4;
             _questcheck.quest3 = true;
             PlayerPrefs.SetInt("quest3", (_questcheck.quest3 ? 1 : 0));
         }
